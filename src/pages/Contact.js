98c80b4ef9601991b2eb1286861532adb49acdc9
Aminat Ahmed <oahmedaminat@gmail.com>
2022-11-04 00:07:47 +0100

    refactor: refactor Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -11,22 +11,26 @@ const Contact = () => {
         <div className="name">
           <div className="first">
             <label>First Name</label>
-            <input type="text" placeholder="   Enter your first name" />
+            <br />
+            <input type="text" placeholder="Enter your first name" />
           </div>
           <div className="last">
             <label>Last Name</label>
-            <input type="text" placeholder="   Enter your last name" />
+            <br />
+            <input type="text" placeholder="Enter your last name" />
           </div>
         </div>
         <label>Email</label>
-        <input type="email" placeholder="   yourname@email.com" />
+        <input type="email" placeholder="yourname@email.com" />
         <label>Message</label>
         <textarea placeholder="Send me a message and I'll reply you as soon as possible..."></textarea>
-        <input type="checkbox" />{" "}
-        <label>
-          You agree to providing your data to Aminat who may contact you.
-        </label>
-        <button>Send message</button>
+        <div className="check">
+          <input type="checkbox" />
+          <label>
+            You agree to providing your data to Aminat who may contact you.
+          </label>
+        </div>
+        <button className="btn">Send message</button>
       </form>
       <Footer />
     </div>
